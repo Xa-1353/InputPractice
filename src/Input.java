f4b29df103b48743457f4030b4fca8ab90a15bc5
Xaria A <xaria.shella@gmail.com>
2023-09-15 09:28:54 -0500

    Finished adding short and byte variables

---
+++ src/Input.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+public class Input {
+
+        //(c) A+ Computer Science
+//www.apluscompsci.com
+//NAME -
+//DATE -
+//PURPOSE -
+
+
+
+
+            public static void main (String[] args)
+            {
+                Scanner keyboard = new Scanner(System.in);
+
+                int intOne, intTwo;
+                byte byteOne, byteTwo;
+                short shortOne, shortTwo;
+                //add more variables of at least 2 different types
+
+
+                System.out.print("Enter an integer :: ");
+                intOne = keyboard.nextInt();
+
+
+                System.out.print("Enter an integer :: ");
+                intTwo = keyboard.nextInt();
+
+                System.out.print("Enter an byte :: ");
+                byteOne = keyboard.nextByte();
+                System.out.print("Enter an byte :: ");
+                byteTwo = keyboard.nextByte();
+
+                System.out.print("Enter an short :: ");
+                shortOne = keyboard.nextShort();
+                System.out.print("Enter an short :: ");
+                shortTwo = keyboard.nextShort();
+
+
+                //add in input for all variables
+
+
+                System.out.println();
+                System.out.println("integer one = " + intOne );
+                System.out.println("integer two = " + intTwo );
+                System.out.println("byte one = " + byteOne );
+                System.out.println("byte two = " + byteTwo );
+                System.out.println("short one = " + shortOne );
+                System.out.println("short two = " + shortTwo );
+
+                //add output for all variables
+
+
+            }
+        }
